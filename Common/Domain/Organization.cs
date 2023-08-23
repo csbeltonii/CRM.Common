159bab5c1d235110ac6e12f9cb1752885852a69d
Craig Belton II <csbeltonii@gmail.com>
2023-08-23 19:03:44 -0400

    added organization

---
+++ Common/Domain/Organization.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace Common.Domain;
+
+public class Organization
+{
+    public string Id { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public DateTime LastUpdated { get; set; }
+
+    [JsonProperty("_etag")]
+    public string Etag { get; set; }
+}
