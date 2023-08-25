a3c94b0414d47e6b41a9130c4c32eb23c8397e00
Craig Belton II <csbeltonii@gmail.com>
2023-08-24 21:28:04 -0400

    added basic Get

--- Common/Repositories/IRepository.cs
+++ Common/Repositories/IRepository.cs
@@ -6,12 +6,18 @@ public interface IRepository<TEntity>
     where TEntity : class
 {
     Task<TEntity> Create(TEntity entity, string partitionKey, CancellationToken cancellationToken);
+
     Task<TEntity> Get(string id, string partitionKey, CancellationToken cancellationToken);
+
     Task<IEnumerable<TEntity>> Get(string searchCriteria, CancellationToken cancellationToken);
+
+    Task<IEnumerable<TEntity>> Get(Expression<Func<TEntity, bool>> predicate, CancellationToken cancellationToken);
 
     Task<IEnumerable<TEntityDto>> Get<TEntityDto>(Expression<Func<TEntity, bool>> predicate,
                                                   Expression<Func<TEntity, TEntityDto>> selector,
                                                   CancellationToken cancellationToken);
+
     Task<TEntity> Update(TEntity entity, string etag, string partitionKey, CancellationToken cancellationToken);
+
     Task<bool> Delete(string id, string partitionKey, CancellationToken cancellationToken);
 }
