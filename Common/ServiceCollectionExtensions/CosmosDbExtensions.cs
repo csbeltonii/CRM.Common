04f0a1adda28955d7e563c44fd895fef127cf2d5
Craig Belton II <csbeltonii@gmail.com>
2023-08-30 21:12:36 -0400

    added cosmos db extension method

---
+++ Common/ServiceCollectionExtensions/CosmosDbExtensions.cs
@@ -0,0 +1,34 @@
+﻿using Common.Utility;
+using Microsoft.Azure.Cosmos;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Common.ServiceCollectionExtensions;
+
+public static class CosmosDbExtensions
+{
+    public static IServiceCollection AddCosmosDb(this IServiceCollection services, IConfiguration configuration)
+    {
+        var cosmosConnectionString = configuration["CosmosDbConnectionString"];
+
+        var cosmosOptions = new CosmosClientOptions
+        {
+            ConnectionMode = ConnectionMode.Gateway,
+            SerializerOptions = new CosmosSerializationOptions
+            {
+                IgnoreNullValues = true,
+                PropertyNamingPolicy = CosmosPropertyNamingPolicy.CamelCase
+            }
+        };
+
+        services
+            .AddOptions()
+            .Configure<DatabaseSettings>(configuration.GetSection("DatabaseSettings"));
+
+        services
+            .AddSingleton(new CosmosClient(cosmosConnectionString, cosmosOptions));
+
+        return services;
+    }
+}
+
